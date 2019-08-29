283e4a83c952fb58d825f69a9a8ab7422cc8adb6
Na Wu <nightcatwu@gmail.com>
2019-08-29 14:53:34 -0400

    Leet code 1160
    1160. Find Words That Can Be Formed by Characters

---
+++ src/easy/Leetcode1160.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+
+public class Leetcode1160 {
+
+    private static boolean match;
+
+    static public int countCharacters(String[] words, String chars) {
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        Arrays.stream(words).forEach(s -> {
+            match = true;
+            StringBuilder tempChars = new StringBuilder(chars);
+            s.chars().forEach(c -> {
+                int pos = tempChars.indexOf(String.valueOf((char)c));
+                if (pos < 0) {
+                    match = false;
+                } else {
+                    tempChars.deleteCharAt(pos);
+                }
+            });
+            if (match) {
+                stringBuilder.append(s);
+            }
+        });
+
+        return stringBuilder.length();
+    }
+
+    static public void  main (String[] args) {
+
+        String[] words = new String[] {"dyiclysmffuhibgfvapygkorkqllqlvokosagyelotobicwcmebnpznjbirzrzsrtzjxhsfpiwyfhzyonmuabtlwin","ndqeyhhcquplmznwslewjzuyfgklssvkqxmqjpwhrshycmvrb","ulrrbpspyudncdlbkxkrqpivfftrggemkpyjl","boygirdlggnh","xmqohbyqwagkjzpyawsydmdaattthmuvjbzwpyopyafphx","nulvimegcsiwvhwuiyednoxpugfeimnnyeoczuzxgxbqjvegcxeqnjbwnbvowastqhojepisusvsidhqmszbrnynkyop","hiefuovybkpgzygprmndrkyspoiyapdwkxebgsmodhzpx","juldqdzeskpffaoqcyyxiqqowsalqumddcufhouhrskozhlmobiwzxnhdkidr","lnnvsdcrvzfmrvurucrzlfyigcycffpiuoo","oxgaskztzroxuntiwlfyufddl","tfspedteabxatkaypitjfkhkkigdwdkctqbczcugripkgcyfezpuklfqfcsccboarbfbjfrkxp","qnagrpfzlyrouolqquytwnwnsqnmuzphne","eeilfdaookieawrrbvtnqfzcricvhpiv","sisvsjzyrbdsjcwwygdnxcjhzhsxhpceqz","yhouqhjevqxtecomahbwoptzlkyvjexhzcbccusbjjdgcfzlkoqwiwue","hwxxighzvceaplsycajkhynkhzkwkouszwaiuzqcleyflqrxgjsvlegvupzqijbornbfwpefhxekgpuvgiyeudhncv","cpwcjwgbcquirnsazumgjjcltitmeyfaudbnbqhflvecjsupjmgwfbjo","teyygdmmyadppuopvqdodaczob","qaeowuwqsqffvibrtxnjnzvzuuonrkwpysyxvkijemmpdmtnqxwekbpfzs","qqxpxpmemkldghbmbyxpkwgkaykaerhmwwjonrhcsubchs"};
+        String chars = "usdruypficfbpfbivlrhutcgvyjenlxzeovdyjtgvvfdjzcmikjraspdfp";
+        System.out.println(countCharacters(words, chars));
+
+    }
+}
